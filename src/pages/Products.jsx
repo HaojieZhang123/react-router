782d08cbd64aba75f27536e4bf5398db5bccfc3b
HaojieZhang123 <haojiezhang123@gmail.com>
2025-05-15 16:28:00 +0200

    [FEAT] fetched products from fakestoreapi and displayed them in product page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Products = () => {
+
+    const [products, setProducts] = useState([])
+
+    const fetchProducts = () => {
+        axios.get('https://fakestoreapi.com/products')
+            .then((response) => {
+                setProducts(response.data)
+            })
+            .catch((error) => {
+                console.error('Error fetching products:', error)
+            })
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
     return (
         <>
             <Navbar />
             <h1>Products</h1>
+
+            <div className="products">
+                {products.map((product) => (
+                    <div key={product.id} className="product">
+                        <h2>{product.title}</h2>
+                        <img src={product.image} alt={product.title} />
+                        <p>{product.description}</p>
+                        <p>Price: ${product.price}</p>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
